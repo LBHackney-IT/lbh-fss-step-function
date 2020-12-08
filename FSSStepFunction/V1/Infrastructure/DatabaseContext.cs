dbe70c948875a248b9593ced639c18de12e7fa1d
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-12-08 16:25:56 +0000

    connection string is correctly set in gateway.  no need to do it in the db context.

--- FSSStepFunction/V1/Infrastructure/DatabaseContext.cs
+++ FSSStepFunction/V1/Infrastructure/DatabaseContext.cs
@@ -22,7 +22,8 @@ namespace LbhFssStepFunction.V1.Infrastructure
             if (!optionsBuilder.IsConfigured)
             {
                 // warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http: //go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+                optionsBuilder.UseNpgsql(
+                    "Host=localhost;Database=fss-public_dev;Username=postgres;Password=mypassword");
             }
         }
 
