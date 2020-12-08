2747e8f6d14991201374b157df2152db7c4f669e
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-12-08 16:43:36 +0000

    make id property consistent between requests and responses.

--- FSSStepFunction/V1/Boundary/Responses/OrganisationResponse.cs
+++ FSSStepFunction/V1/Boundary/Responses/OrganisationResponse.cs
@@ -4,7 +4,7 @@ namespace LbhFssStepFunction.V1
 {
     public class OrganisationResponse
     {
-        public int Id { get; set; }
+        public int OrganisationId { get; set; }
         public string OrganisationName { get; set; }
         public string Status { get; set; }
         public List<string> EmailAddresses { get; set; }
