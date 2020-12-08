8b111ebc2fb0e41b77824083e8f405b840b37525
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-12-08 16:26:17 +0000

    omitting sub resources.

--- FSSStepFunction/V1/Gateways/OrganisationsGateway.cs
+++ FSSStepFunction/V1/Gateways/OrganisationsGateway.cs
@@ -28,10 +28,10 @@ namespace LbhFssStepFunction.V1.Gateways
         {
             try
             {
-                LoggingHandler.LogInfo("Initiating database query");
+                LoggingHandler.LogInfo($"Initiating database query for organisation with id {id}");
                 var organisation = _context.Organisations
-                    .Include(o => o.UserOrganisations)
-                    .ThenInclude(uo => uo.User)
+                    // .Include(o => o.UserOrganisations)
+                    // .ThenInclude(uo => uo.User)
                     .FirstOrDefault(o => o.Id == id);
                 return organisation.ToDomain();
             }
