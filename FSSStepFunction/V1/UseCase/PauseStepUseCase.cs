b61cef2db2573b607a3206fd6da0743b7585293f
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-27 15:50:35 +0000

    set up use case for gateway injections and call organisation gateway pause organisation method

--- FSSStepFunction/V1/UseCase/PauseStepUseCase.cs
+++ FSSStepFunction/V1/UseCase/PauseStepUseCase.cs
@@ -12,17 +12,21 @@ namespace LbhFssStepFunction.V1.UseCase
         private readonly IOrganisationsGateway _organisationsGateway;
         private readonly INotifyGateway _notifyGateway;
 
-        public PauseStepUseCase()
+        public PauseStepUseCase(IOrganisationsGateway organisationsGateway = null, INotifyGateway notifyGateway = null)
         {
-            _organisationsGateway = new OrganisationsGateway();
-            _notifyGateway = new NotifyGateway();
+            _organisationsGateway = organisationsGateway ?? new OrganisationsGateway();
+            _notifyGateway = notifyGateway ?? new NotifyGateway();
         }
         public async Task<OrganisationResponse> GetOrganisationAndSendEmail(int id)
         {
             LoggingHandler.LogInfo("Second step executing request to gateway to get organisation");
-            var organisation = _organisationsGateway.GetOrganisationById(id).ToResponse();
-            if (organisation == null)
+            var org = _organisationsGateway.GetOrganisationById(id);
+            if (org == null)
                 return null;
+            var updatedOrg = _organisationsGateway.PauseOrganisation(org.Id);
+            if (updatedOrg == null)
+                return null;
+            var organisation = updatedOrg.ToResponse();
             await _notifyGateway.SendNotificationEmail(organisation.OrganisationName, organisation.EmailAddresses.ToArray(), 4).ConfigureAwait(true);
             return organisation;
         }
