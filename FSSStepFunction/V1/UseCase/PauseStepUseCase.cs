bfb8f2d08d93cf14afe489688c99cbd236441be9
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-01-27 12:16:18 +0000

    make gateway method async

--- FSSStepFunction/V1/UseCase/PauseStepUseCase.cs
+++ FSSStepFunction/V1/UseCase/PauseStepUseCase.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using LbhFssStepFunction.V1.Factories;
 using LbhFssStepFunction.V1.Gateways;
 using LbhFssStepFunction.V1.Gateways.Interface;
@@ -8,7 +9,7 @@ namespace LbhFssStepFunction.V1.UseCase
 {
     public class PauseStepUseCase : IPauseStepUseCase
     {
-        private readonly IOrganisationGateway _organisationsGateway;
+        private readonly IOrganisationsGateway _organisationsGateway;
         private readonly INotifyGateway _notifyGateway;
 
         public PauseStepUseCase()
@@ -16,13 +17,13 @@ namespace LbhFssStepFunction.V1.UseCase
             _organisationsGateway = new OrganisationsGateway();
             _notifyGateway = new NotifyGateway();
         }
-        public OrganisationResponse GetOrganisationAndSendEmail(int id)
+        public async Task<OrganisationResponse> GetOrganisationAndSendEmail(int id)
         {
             LoggingHandler.LogInfo("Second step executing request to gateway to get organisation");
             var organisation = _organisationsGateway.GetOrganisationById(id).ToResponse();
             if (organisation == null)
                 return null;
-            _notifyGateway.SendNotificationEmail(organisation.EmailAddresses.ToArray(), 4);
+            await _notifyGateway.SendNotificationEmail(organisation.OrganisationName, organisation.EmailAddresses.ToArray(), 4).ConfigureAwait(true);
             return organisation;
         }
     }
