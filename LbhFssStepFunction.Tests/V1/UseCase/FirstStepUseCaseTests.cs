334f4a3bffe24773cf65cefc210b6ccdef6ffc3c
duslerke <43747286+Duslerke@users.noreply.github.com>
2022-03-01 13:07:47 +0000

    Fixed existing First Step UC tests.
    The tests weren't awaiting for UC to execute & as a result would finish executing successfully before anything within UC call would happen.

--- LbhFssStepFunction.Tests/V1/UseCase/FirstStepUseCaseTests.cs
+++ LbhFssStepFunction.Tests/V1/UseCase/FirstStepUseCaseTests.cs
@@ -1,9 +1,11 @@
+using System.Threading.Tasks;
 using LbhFssStepFunction.Tests.TestHelpers;
 using LbhFssStepFunction.V1.Factories;
 using LbhFssStepFunction.V1.Gateways.Interface;
 using LbhFssStepFunction.V1.UseCase;
 using Moq;
 using NUnit.Framework;
+using System;
 
 namespace LbhFssStepFunction.Tests.V1.UseCase
 {
@@ -17,24 +19,37 @@ namespace LbhFssStepFunction.Tests.V1.UseCase
         [SetUp]
         public void Setup()
         {
+            Environment.SetEnvironmentVariable("WAIT_DURATION", "600");
             _mockOrganisationGateway = new Mock<IOrganisationsGateway>();
             _mockNotifyGateway = new Mock<INotifyGateway>();
             _classUnderTest = new FirstStepUseCase(_mockOrganisationGateway.Object, _mockNotifyGateway.Object);
         }
 
-        [TestCase(TestName = "Given that the first step use case gets called, it calls the organisation gateway GetOrganisationsToReview method.")]
-        public void FirstStepUseCaseCallsOrganisationGateway()
+        [TestCase(TestName = @"
+            Given that the first step use case gets called,
+            It calls the organisation gateway GetOrganisationsToReview method.")]
+        public async Task FirstStepUseCaseCallsOrganisationGateway()
         {
-            _classUnderTest.GetOrganisationAndSendEmail(1);
+            // act
+            await _classUnderTest.GetOrganisationAndSendEmail(1);
+            
+            // assert
             _mockOrganisationGateway.Verify(gw => gw.GetOrganisationById(It.IsAny<int>()), Times.Once);
         }
 
-        [TestCase(TestName = "Given that the first step use case gets called, it calls the notify gateway SendNotificationEmail method.")]
-        public void FirstStepUseCaseCallsNotificationGateway()
+        [TestCase(TestName = @"
+            Given that the first step use case gets called,
+            It calls the notify gateway SendNotificationEmail method.")]
+        public async Task FirstStepUseCaseCallsNotificationGateway()
         {
+            // arrange
             var organisation = EntityHelpers.CreateOrganisation();
             _mockOrganisationGateway.Setup(og => og.GetOrganisationById(It.IsAny<int>())).Returns(organisation.ToDomain());
-            _classUnderTest.GetOrganisationAndSendEmail(1);
+
+            // act
+            await _classUnderTest.GetOrganisationAndSendEmail(1);
+            
+            // assert
             _mockNotifyGateway.Verify(gw => gw.SendNotificationEmail(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<int>()), Times.Once);
         }
 
