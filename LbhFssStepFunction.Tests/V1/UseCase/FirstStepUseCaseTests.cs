cf151d202242c26bc929928ef7985ce209adb5fb
duslerke <43747286+Duslerke@users.noreply.github.com>
2022-03-03 14:46:39 +0000

    Made the First Step UC method trigger the flagging of organisation as in revalidation/reverification process.

--- LbhFssStepFunction.Tests/V1/UseCase/FirstStepUseCaseTests.cs
+++ LbhFssStepFunction.Tests/V1/UseCase/FirstStepUseCaseTests.cs
@@ -6,6 +6,8 @@ using LbhFssStepFunction.V1.UseCase;
 using Moq;
 using NUnit.Framework;
 using System;
+using LbhFssStepFunction.V1.Errors;
+using FluentAssertions;
 
 namespace LbhFssStepFunction.Tests.V1.UseCase
 {
@@ -53,5 +55,43 @@ namespace LbhFssStepFunction.Tests.V1.UseCase
             _mockNotifyGateway.Verify(gw => gw.SendNotificationEmail(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<int>()), Times.Once);
         }
 
+        // There should be a test case for UC return, but the current return doesn't make sense.
+        // Will look into refactoring in the future.
+
+        [TestCase(TestName = @"
+            Given an existing organisation's Id (GW mock will not throw an error),
+            When the first step usecase's GetOrganisationAndSendEmail method is called,
+            Then the secondary call to FlagOrganisationToBeInRevalidation organition gateway's method is made")]
+        public async Task FirstStepUCCallsFlagOrgMethod()
+        {
+            // arrange
+            int randomOrgId = Randomm.Id(100, 200);
+
+            // act
+            // _mockOrganisationGateway.Setup(gw => gw.FlagOrganisationToBeInRevalidation(It.IsAny<int>()));
+            await _classUnderTest.GetOrganisationAndSendEmail(randomOrgId);
+
+            // assert
+            _mockOrganisationGateway.Verify(gw => gw.FlagOrganisationToBeInRevalidation(It.IsAny<int>()), Times.Once);
+        }
+
+        [TestCase(TestName = @"
+            Given an existing organisation's Id (GW mock will not throw an error),
+            When the first step usecase's GetOrganisationAndSendEmail method is called,
+            Then the secondary call to FlagOrganisationToBeInRevalidation organition gateway's method is made")]
+        public async Task FirstStepUCReturnsNullWhenFlagOrgMethodThrowsResourceNotFound()
+        {
+            // arrange
+            int randomOrgId = Randomm.Id(100, 200);
+            _mockOrganisationGateway.Setup(gw => gw.FlagOrganisationToBeInRevalidation(It.IsAny<int>()))
+                .Throws(new ResourceNotFoundException("Error"));
+
+            // act
+            var ucResult = await _classUnderTest.GetOrganisationAndSendEmail(randomOrgId);
+
+            // assert
+            _mockOrganisationGateway.Verify(gw => gw.FlagOrganisationToBeInRevalidation(It.IsAny<int>()), Times.Once);
+            ucResult.Should().Be(null);
+        }
     }
 }
