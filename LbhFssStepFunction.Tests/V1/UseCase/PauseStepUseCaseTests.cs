d20e696890559f8bbdf1a8a9c37309af567427ab
duslerke <43747286+Duslerke@users.noreply.github.com>
2022-03-16 17:50:43 +0000

    Made the Pause Step terminate when an Organisation has been updated since step 3
    Also improved and expanded on existing Pause UC tests and edge cases. There could also be logging tests, however, that's low priority work that shouldn't be done right now.

--- LbhFssStepFunction.Tests/V1/UseCase/PauseStepUseCaseTests.cs
+++ LbhFssStepFunction.Tests/V1/UseCase/PauseStepUseCaseTests.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
 using LbhFssStepFunction.Tests.TestHelpers;
+using LbhFssStepFunction.V1.Domains;
+using LbhFssStepFunction.V1.Errors;
 using LbhFssStepFunction.V1.Factories;
 using LbhFssStepFunction.V1.Gateways.Interface;
 using LbhFssStepFunction.V1.UseCase;
@@ -22,30 +28,202 @@ namespace LbhFssStepFunction.Tests.V1.UseCase
             _classUnderTest = new PauseStepUseCase(_mockOrganisationGateway.Object, _mockNotifyGateway.Object);
         }
 
-        [TestCase(TestName = "Given that the pause step use case gets called, it calls the organisation gateway GetOrganisationsToReview method.")]
-        public void PauseStepUseCaseCallsOrganisationGateway()
+        [TestCase(TestName = @"
+            Given any input organisation id,
+            When the Pause Step use case gets called,
+            Then it calls the GetOrganisationsToReview organisation gateway method
+            With correct parameters.")]
+        public async Task PauseStepUseCaseCallsOrganisationGateway()
         {
-            _classUnderTest.GetOrganisationAndSendEmail(1);
-            _mockOrganisationGateway.Verify(gw => gw.GetOrganisationById(It.IsAny<int>()), Times.Once);
+            // arrange
+            int anyId = Randomm.Id();
+
+            // act
+            await _classUnderTest.GetOrganisationAndSendEmail(anyId);
+            
+            // assert
+            _mockOrganisationGateway.Verify(
+                gw => gw.GetOrganisationById(It.Is<int>(id => id == anyId)),
+                Times.Once);
         }
 
-        [TestCase(TestName = "Given that the pause step use case gets called, it calls the organisation gateway PauseOrganisation method.")]
-        public void PauseStepUseCaseCallsOrgnisationGatewayPauseMethod()
+        [TestCase(TestName = @"
+            Given an existing Organisation Id,
+            And that organisation being in Revalidation process,
+            When the Pause Step use case gets called,
+            Then it calls the PauseOrganisation organisation gateway method with Correct parameters.")]
+        public async Task PauseStepUseCaseCallsOrgnisationGatewayPauseMethodWithCorrectParameters()
         {
-            var organisation = EntityHelpers.CreateOrganisation();
-            _mockOrganisationGateway.Setup(og => og.GetOrganisationById(It.IsAny<int>())).Returns(organisation.ToDomain());
-            _classUnderTest.GetOrganisationAndSendEmail(1);
-            _mockOrganisationGateway.Verify(gw => gw.PauseOrganisation(It.IsAny<int>()), Times.Once);
+            // arrange
+            var organisation = EntityHelpers.CreateOrganisationWithUsers(withIds: true);
+            int existingId = organisation.Id;
+            organisation.InRevalidationProcess = true;
+
+            var domainOrganisation = organisation.ToDomain();
+            
+            _mockOrganisationGateway
+                .Setup(og => og.GetOrganisationById(It.Is<int>(id => id == existingId)))
+                .Returns(domainOrganisation);
+
+            // act
+            await _classUnderTest.GetOrganisationAndSendEmail(existingId);
+            
+            // assert
+            _mockOrganisationGateway.Verify(
+                gw => gw.PauseOrganisation(It.Is<int>(id => id == existingId)),
+                Times.Once);
         }
 
-        [TestCase(TestName = "Given that the pause step use case gets called, it calls the notify gateway SendNotificationEmail method.")]
-        public void PauseStepUseCaseCallsNotificationGateway()
+        [TestCase(TestName = @"
+            Given an existing Organisation Id,
+            And that organisation being in Revalidation process, 
+            When the Pause Step use case gets called,
+            Then it calls the SendNotificationEmail notify gateway method with correct parameters,
+            And it returns the correct organisation & Next Step state data.")]
+        public async Task PauseStepUseCaseCallsNotificationGatewayWithCorrectParamsIfOrganisationWasNotUpdated()
         {
-            var organisation = EntityHelpers.CreateOrganisation();
-            _mockOrganisationGateway.Setup(og => og.GetOrganisationById(It.IsAny<int>())).Returns(organisation.ToDomain());
-            _classUnderTest.GetOrganisationAndSendEmail(1);
-            _mockNotifyGateway.Verify(gw => gw.SendNotificationEmail(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<int>()), Times.Once);
+            // arrange
+            var organisation = EntityHelpers.CreateOrganisationWithUsers(withIds: true, activeUsers: true);
+            int existingId = organisation.Id;
+            // Implies that organisation wasn't updated since Step 3 was called.
+            organisation.InRevalidationProcess = true;
+
+            var expectedNotifyEmailArgs = organisation.UserOrganisations.Select(uo => uo.User.Email).ToList();
+
+            var domainOrganisation = organisation.ToDomain();
+            
+            _mockOrganisationGateway
+                .Setup(og => og.GetOrganisationById(It.Is<int>(id => id == existingId)))
+                .Returns(domainOrganisation);
+            
+            // act
+            var ucResult = await _classUnderTest.GetOrganisationAndSendEmail(existingId);
+            
+            // assert
+            _mockNotifyGateway.Verify(
+                gw => gw.SendNotificationEmail(
+                    It.Is<string>(n => n == domainOrganisation.Name), 
+                    It.Is<string[]>(es => 
+                        es.All(e => expectedNotifyEmailArgs.Contains(e)) && 
+                        es.Count() == expectedNotifyEmailArgs.Count()), 
+                    It.Is<int>(state => state == 4)), 
+                Times.Once);
+
+            ucResult.Should().NotBeNull();
+            ucResult.EmailAddresses.Should().BeEquivalentTo(expectedNotifyEmailArgs);
+            // Next step time should be ±2 seconds from the time the UC was executed. Should be accurate enough for testing purposes.
+            ucResult.StateResult.Should().BeTrue(); // indicate success //(default(bool));
+            ucResult.OrganisationId.Should().Be(existingId); // the rest of the organisations data is irrelevant.
+            ucResult.NextStepTime.Should().BeCloseTo(default(DateTime), precision: 2000); // shows datetime is irrelevant
         }
 
+        // existing not valid
+        [TestCase(TestName = @"
+            Given an existing Organisation Id,
+            And that organisation NOT being in Revalidation process,
+            When the Pause Step use case gets called,
+            Then it does NOT call the SendNotfication notify gateway's method,
+            And it returns a NULL result.")]
+        public async Task PauseStepUCDoesNotCallNotifyGWAndReturnsNullWhenOrganisationWasUpdated()
+        {
+            // arrange
+            var organisation = EntityHelpers.CreateOrganisationWithUsers(withIds: true, activeUsers: true);
+            int existingId = organisation.Id;
+            
+            // This being false implies that organisation was updated between the Step 3 and Pause Step.
+            organisation.InRevalidationProcess = false;
+
+            var domainOrganisation = organisation.ToDomain();
+
+            _mockOrganisationGateway
+                .Setup(gw => gw.GetOrganisationById(It.Is<int>(id => id == existingId)))
+                .Returns(domainOrganisation);
+
+            // act
+            var ucResult = await _classUnderTest.GetOrganisationAndSendEmail(existingId);
+
+            // assert
+            _mockNotifyGateway.Verify(
+                gw => gw.SendNotificationEmail(
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>(),
+                    It.IsAny<int>()),
+                Times.Never);
+
+            ucResult.Should().BeNull();
+            // I think in this particular case it would make more sense to return 'false'
+            // since that's not so much an error, but rather an invalid operation.
+        }
+
+        [TestCase(TestName = @"
+            Given a NOT existing Organisation Id,
+            When the Pause Step Usecase gets called,
+            Then it does NOT call SendNotfication notify gateway's method,
+            And it returns a NULL result.")]
+        public async Task PauseStepUCDoesNotCallNotifyGWAndReturnsNullWhenOrganisationIsNotFound()
+        {
+            // arrange
+            int nonExistingId = Randomm.Id();
+            // organisation was deleted between the triggering of Step 3 and Pause Step.
+            OrganisationDomain organisation = null;
+
+            _mockOrganisationGateway
+                .Setup(gw => gw.GetOrganisationById(It.Is<int>(id => id == nonExistingId)))
+                .Returns(organisation);
+
+            // act
+            var ucResult = await _classUnderTest.GetOrganisationAndSendEmail(nonExistingId);
+
+            // assert
+            _mockNotifyGateway.Verify(
+                gw => gw.SendNotificationEmail(
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>(),
+                    It.IsAny<int>()),
+                Times.Never);
+            
+            ucResult.Should().BeNull();
+            // This one is well deserved NULL since we don't even know what the organisation is anymore.
+        }
+
+        [TestCase(TestName = @"
+            Given an existing organisation's Id,
+            In an unlikely case of organisation getting deleted or inaccessible immediatelly after retrieving it,
+            When the Pause Step use case's GetOrganisationAndSendEmail method is called,
+            And UC calls PauseOrganisation notify GW method, which throws an exception as a result,
+            Then the exception gets caught and NULL is returned.")]
+        public void PauseStepUCReturnsThrowsWhenPauseOrgMethodThrowsResourceNotFound()
+        {
+            // arrange
+            var organisation = EntityHelpers.CreateOrganisationWithUsers(withIds: true, activeUsers: true);
+            int existingId = organisation.Id;
+            organisation.InRevalidationProcess = true; // we don't terminate immediatelly
+            var domainOrganisation = organisation.ToDomain();
+
+            _mockOrganisationGateway
+                .Setup(gw => gw.GetOrganisationById(It.Is<int>(id => id == existingId)))
+                .Returns(domainOrganisation);
+
+            int nonExistingId = existingId; // organisation gets deleted or inaccessible.
+            
+            _mockOrganisationGateway.Setup(gw => gw.PauseOrganisation(It.Is<int>(id => id == nonExistingId)))
+                .Throws(new ResourceNotFoundException("Error"));
+            
+            // act
+            Func<Task> ucCall = async () => await _classUnderTest.GetOrganisationAndSendEmail(existingId);
+
+            // assert
+            _mockNotifyGateway.Verify(
+                gw => gw.SendNotificationEmail(
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>(),
+                    It.IsAny<int>()),
+                Times.Never);
+            
+            ucCall.Should().Throw<ResourceNotFoundException>().WithMessage("Error");
+            // Throw exception back to ensure that retry is scheduled.
+        }
+
+        // There should also be logging tests, however this would be going too far in terms of the ticket work
     }
 }
